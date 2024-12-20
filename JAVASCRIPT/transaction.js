da49074809dca8a3fd189fabaa169df188a11e64
Ayyappa Gandikota <coder.honey.915@gmail.com>
2024-12-20 23:00:59 +0530

    Update transaction.js

--- JAVASCRIPT/transaction.js
+++ JAVASCRIPT/transaction.js
@@ -1,5 +1,4 @@
-function transaction(event)
- {
+function transaction(event) {
     event.preventDefault();
     const account_number = document.getElementById("account_number").value;
     const pin_number = document.getElementById("pin_number").value;
@@ -7,29 +6,43 @@ function transaction(event)
         alert("Enter All The Details...");
         return;
     }
-    if (!/^\d{10}$/.test(account_number))
-    {
-        document.getElementById('msg1').innerHTML="Account number must contain exactly 10 DIGITS only";
+    if (!/^\d{10}$/.test(account_number)) {
+        document.getElementById('msg1').innerHTML = "Account number must contain exactly 10 DIGITS only";
         return;
-    }
-    if (/^\d{10}$/.test(account_number))  { document.getElementById('msg1').innerHTML=""; }
-    if (!/^\d{4}$/.test(pin_number)) 
+    } 
+    else 
     {
-        document.getElementById('msg2').innerHTML="Pin number must contain exactly 4 DIGITS";
+        document.getElementById('msg1').innerHTML = "";
+    }
+    if (!/^\d{4}$/.test(pin_number)) {
+        document.getElementById('msg2').innerHTML = "Pin number must contain exactly 4 DIGITS";
         return;
-    }
-    if (/^\d{4}$/.test(pin_number))  { document.getElementById('msg2').innerHTML=""; }
-    fetch('/transaction',
+    } 
+    else 
     {
+        document.getElementById('msg2').innerHTML = "";
+    }
+    fetch('/transaction', {
         method: "POST",
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ account_number, pin_number })
     })
     .then((res) => res.json())
-    .then((result)=>{
-        alert(result.msg);
-        if(result.done) {window.location.href='/index.html';}
+    .then((result) => {
+        if (result.done) { 
+            const transactions = result.tra.map((doc) => {
+                ` ----->  Account Number: ${doc.account_number}, Type: ${doc.type}, Amount: ${doc.amount}, Date: ${doc.date}`}).join('\n')
+            alert(`Transaction History:\n \n ${transactions}`);
+            window.location.href = '/index.html';
+        } 
+        else 
+        {
+            alert("Failed to fetch transaction history.");
+        }
     })
+    .catch((error) => {
+        alert("An error occurred while fetching transaction history.",error);
+    });
 }
 
 function clearFields() {
